2c6ed4273f10956dbb74e6498de6239e48a3eb8d
tweezerchef <ltomblock@gmail.com>
2024-04-10 10:07:57 -0500

    added simple

--- Algo/LinkedList/insert_node.py
+++ Algo/LinkedList/insert_node.py
@@ -29,3 +29,23 @@ def insert_node(head, value, index):
             current = current.next
             count += 1
     return head
+
+
+# simpler solution
+def insert_node_1(head, value, index):
+    if index == 0:
+        new_head = Node(value)
+        new_head.next = head
+        return new_head
+
+    count = 0
+    current = head
+    while current is not None:
+        if count == index - 1:
+            temp = current.next
+            current.next = Node(value)
+            current.next.next = temp
+
+        count += 1
+        current = current.next
+    return head
